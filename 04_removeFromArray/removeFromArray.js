45f028327d9eb1bb8a4e6ba35363fc3aa40dbad5
Hypxr-7 <Hypxr-7@users.noreply.github.com>
2024-09-17 19:11:34 +0500

    Add solution for 04/

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    let newArr = [];
+    for (const i of arr){
+        let flag = false;
+        for (const j of args) {
+            if (j === i) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag) continue;
+        newArr.push(i);
+    }
+    return newArr
 };
 
 // Do not edit below this line
